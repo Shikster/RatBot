83ef33ca140fc0bccad82ffc488f779cc29b62f4
Shiki <shikirs07@gmail.com>
2023-10-10 00:32:15 +0200

    Update RatBot.user.js

--- RatBot.user.js
+++ RatBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RatBot
 // @namespace    https://github.com/Shikster/RatBot.git
-// @version      1.6
+// @version      1.7
 // @description  Eep! I found someone!
 // @author       Shikii
 // @match        https://aberoth.com/*
@@ -10,17 +10,7 @@
 // @grant        GM_setValue
 // ==/UserScript==
 
-//V3
-function shouldDetect(username) {
-    if (useNameList) {
-        return !nameList.includes(username); // Return true if the username is in the nameList
-    } else {
-        return true; // Always detect if the whitelist is not used
-    }
-}
-//V3
-
-
+// Function to display a popup for adding names to the nameList
 function displayPasteNamesPopup() {
     const popupContainer = document.createElement('div');
     popupContainer.style.position = 'fixed';
@@ -36,7 +26,7 @@ function displayPasteNamesPopup() {
 
     const closeButton = document.createElement('button');
     closeButton.textContent = 'Close';
-    closeButton.style.marginBottom = '10px'; 
+    closeButton.style.marginBottom = '10px';
     popupContainer.appendChild(closeButton);
 
     const textArea = document.createElement('textarea');
@@ -48,13 +38,12 @@ function displayPasteNamesPopup() {
 
     const addButton = document.createElement('button');
     addButton.textContent = 'Add to NameList';
-    addButton.style.marginTop = '10px'; 
+    addButton.style.marginTop = '10px';
     popupContainer.appendChild(addButton);
-	
+
     closeButton.addEventListener('click', () => {
         document.body.removeChild(popupContainer);
     });
-
 
     addButton.addEventListener('click', () => {
         const usernames = textArea.value.split(',').map(username => username.trim());
@@ -67,6 +56,7 @@ function displayPasteNamesPopup() {
     });
 }
 
+// Function to create the setup menu
 function createSetupMenu() {
     const setupContainer = document.createElement('div');
     setupContainer.style.position = 'fixed';
@@ -82,16 +72,13 @@ function createSetupMenu() {
 
     setupContainer.innerHTML = `
         <!-- Your setup menu HTML -->
-		 <div style="max-height: 500px; overflow-y: auto;">
+        <div style="max-height: 500px; overflow-y: auto;">
         <h2>Information</h2>
          <div>
          <b>AFK ON/OFF:</b> When turned on, it will send a pause key up event every x minutes to prevent camping.
          </div>
          <div>
          <b>DETECT ON/OFF:</b> Turns rat bot on or off.
-         </div>
-         <div>
-         <b>NAMELIST ON/OFF:</b> If on, will use your namelist. If off, detects ALL names on screen. Useful for figuring out who's active when.
          </div>
          <div>
          <b>ADD NAMES:</b> Paste names you want to be detected like this: Name1, Name2, Name3,...
@@ -105,18 +92,16 @@ function createSetupMenu() {
         <label for="myName">Your Scout Name:</label><br>
         <input type="text" id="myName" name="myName" value="${GM_getValue("MY_NAME", "")}" required><br><br>
 
-		<!-- "ADD NAMES" section -->
+        <!-- "ADD NAMES" section -->
         <h2>Add Names</h2>
-		 <div>
-         <b>Adding players:</b> Click the button below to add names to the list you want Rat Bot to scout for. Names on this list will trigger the sound effect and/or send a message to your discord webhook if you have that option enabled.
-		 </div>
+         <div>
+         <b>Adding players:</b> Click the button below to add names the whitelist. Names on this list will be skipped from scouting. Every name that is not on this list will send a message to discord and/or send a sound effect.
+         </div>
         <br>
         <button id="addNamesButton">Add names</button>
-		<br>
         <br>
-		<button id="saveButton">Save</button>
-
-
+        <br>
+        <button id="saveButton">Save</button>
     `;
 
     document.body.appendChild(setupContainer);
@@ -138,7 +123,7 @@ function createSetupMenu() {
     });
 }
 
-
+// Create and initialize setup button and other UI elements
 const setupButton = document.createElement("button");
 setupButton.textContent = "SETUP";
 setupButton.style.width = '80px';
@@ -157,55 +142,47 @@ if (window.location.href === "https://aberoth.com/download.html") {
     createSetupMenu();
 }
 
-
-
-let afkinterval;
-let clicked = true;
-const btn = document.createElement('button');
-btn.textContent = 'AFK ON'; 
-btn.setAttribute('id', 'afkbtn');
-btn.style.width = '80px'; 
-document.body.appendChild(btn);
-
-btn.addEventListener('click', () => {
-    clicked = !clicked;
-    if (clicked) {
-        afkinterval = setInterval(() => {
-            document.body.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true, cancelable: true, keyCode: 19 }));
-            document.body.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: true, keyCode: 19 }));
-        }, 10000);
-    } else {
-        clearInterval(afkinterval);
-        document.body.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: true, keyCode: 19 }));
-    }
-    btn.textContent = clicked ? 'AFK ON' : 'AFK OFF'; 
-});
-
+// Variables for controlling bot behavior
 let isDetecting = true;
 let useNameList = true;
 
+// Toggle buttons for detecting and using the name list
 const toggleBtn = document.createElement('button');
 toggleBtn.textContent = 'DETECTING ON';
 toggleBtn.style.width = '150px';
 document.body.appendChild(toggleBtn);
 
-const useNameListBtn = document.createElement('button');
+/* const useNameListBtn = document.createElement('button');
 useNameListBtn.textContent = 'NAMELIST ON';
 useNameListBtn.style.width = '150px';
 document.body.appendChild(useNameListBtn);
-
+*/ 
 toggleBtn.addEventListener('click', () => {
     isDetecting = !isDetecting;
     toggleBtn.textContent = isDetecting ? 'DETECTING ON' : 'DETECTING OFF';
 });
 
+/*
 useNameListBtn.addEventListener('click', () => {
     useNameList = !useNameList;
     useNameListBtn.textContent = useNameList ? 'NAMELIST ON' : 'NAMELIST OFF';
+    console.log(useNameList);
 });
+*/
 
+// Retrieve the name list from GM storage
 const nameList = GM_getValue("NAME_LIST", []);
 
+// Function to determine if a username should be detected
+function shouldDetect(username) {
+    if (useNameList) {
+        return !nameList.includes(username); // Detect if the username is not in the nameList
+    } else {
+        return true; // Always detect if the whitelist is not used
+    }
+}
+
+// Function to display the name list in a popup
 function displayNameListPopup() {
     const popupContainer = document.createElement('div');
     popupContainer.style.position = 'fixed';
@@ -216,9 +193,9 @@ function displayNameListPopup() {
     popupContainer.style.padding = '20px';
     popupContainer.style.border = '1px solid #ccc';
     popupContainer.style.zIndex = '1000';
-    popupContainer.style.color = 'black'; 
-    popupContainer.style.overflow = 'auto'; 
-    popupContainer.style.maxHeight = '70vh'; 
+    popupContainer.style.color = 'black';
+    popupContainer.style.overflow = 'auto';
+    popupContainer.style.maxHeight = '70vh';
 
     document.body.appendChild(popupContainer);
 
@@ -231,12 +208,11 @@ function displayNameListPopup() {
     popupContainer.appendChild(nameListContainer);
 
     nameList.forEach(name => {
-		
         const listItem = document.createElement('li');
         const nameContainer = document.createElement('div');
         nameContainer.style.display = 'flex';
         nameContainer.style.alignItems = 'center';
-		
+
         const nameElement = document.createElement('span');
         nameElement.textContent = name;
         nameContainer.appendChild(nameElement);
@@ -245,7 +221,6 @@ function displayNameListPopup() {
         removeButton.textContent = 'X';
         removeButton.style.marginLeft = '10px';
         removeButton.addEventListener('click', () => {
-			
             const index = nameList.indexOf(name);
             if (index !== -1) {
                 nameList.splice(index, 1);
@@ -264,6 +239,7 @@ function displayNameListPopup() {
     });
 }
 
+// Button to view the name list
 const viewNameListBtn = document.createElement('button');
 viewNameListBtn.textContent = 'VIEW NAMELIST';
 document.body.appendChild(viewNameListBtn);
@@ -272,7 +248,7 @@ viewNameListBtn.addEventListener('click', () => {
     displayNameListPopup();
 });
 
-
+// Constants and variables for bot behavior
 const MY_NAME = GM_getValue("MY_NAME", "");
 const DISCORDURL = GM_getValue("DISCORDURL", "");
 const COOLDOWN_TIMER = 30 * 60 * 1000; // 30 minute cooldown before a user can be detected again
@@ -280,6 +256,7 @@ let usersInRoom = [];
 let cooldown = new Map();
 let unpingable = [];
 
+// Main loop for detecting users
 setInterval(() => {
     if (!isDetecting) {
         return;
@@ -302,76 +279,45 @@ setInterval(() => {
                 usersInRoom.push(username);
             }
         }
-/*
-         else if (app.game.Bc.DA[key]['color'] === "white") {
-            if (!usersInRoom.includes(username) && shouldDetect (username)) {
-                usersInRoom.push(username);
-            }
-        }
-		*/
     }
 
-
-for (let user of usersInRoom) {
-    if (!cooldown.has(user) && !unpingable.includes(user)) {
-        if (!nameList.includes(user)) { // Check if the user is not in the nameList
-            if (useNameList) {
-                postUser(user);
-            } else if (user !== MY_NAME) {
-                postUser(user);
-            }
-            const currentTime = new Date().toLocaleTimeString();
-            console.log(user, " entered at", currentTime);
-            cooldown.set(user, Date.now());
-        }
-    }
-}
-
-	
-/*
     for (let user of usersInRoom) {
         if (!cooldown.has(user) && !unpingable.includes(user)) {
-
-            if (useNameList) { 
-                if (nameList.some(name => user.startsWith(name))) {
+            if (!nameList.includes(user)) { // Check if the user is not in the nameList
+                if (useNameList) {
                     postUser(user);
-                    const currentTime = new Date().toLocaleTimeString();
-                    console.log(user, " entered at", currentTime);
-                    cooldown.set(user, Date.now());
-                }
-            } else {
-                if (user !== MY_NAME) {
+                } else if (user !== MY_NAME) {
                     postUser(user);
-                    const currentTime = new Date().toLocaleTimeString();
-                    console.log(user, " entered at", currentTime);
-                    cooldown.set(user, Date.now());
                 }
+                const currentTime = new Date().toLocaleTimeString();
+                console.log(user, " entered at", currentTime);
+                cooldown.set(user, Date.now());
             }
         }
     }
-*/
-
 
     for (const [name, timestamp] of cooldown.entries()) {
         if (!usersInRoom.includes(name) && Date.now() - timestamp >= COOLDOWN_TIMER) {
             cooldown.delete(name);
-            unpingable.splice(unpingable.indexOf(name), 1); 
+            unpingable.splice(unpingable.indexOf(name), 1);
             console.log(`${name} can be pinged again at`, new Date().toLocaleTimeString());
         }
     }
 }, 1000);
 
+// Variable for controlling the sound
 let isSoundEnabled = true;
 const soundToggleButton = document.createElement('button');
-soundToggleButton.textContent = 'SOUND IS ON';
+soundToggleButton.textContent = 'SOUND ON';
 soundToggleButton.style.width = '150px';
 document.body.appendChild(soundToggleButton);
 
 soundToggleButton.addEventListener('click', () => {
     isSoundEnabled = !isSoundEnabled;
-    soundToggleButton.textContent = isSoundEnabled ? 'SOUND IS ON' : 'SOUND IS OFF';
+    soundToggleButton.textContent = isSoundEnabled ? 'SOUND ON' : 'SOUND OFF';
 });
 
+// Function to post a user to Discord
 const postUser = (username) => {
     const options = { hour: '2-digit', minute: '2-digit', timeZone: 'UTC', hour12: false };
     const currentTimeHere = new Date().toLocaleTimeString('en-US', { ...options, timeZone: 'Europe/Berlin' });
