8deb71c68138c445aa632063460ae0464a84277a
Shiki <shikirs07@gmail.com>
2023-10-11 18:31:41 +0200

    Update RatBot.user.js

--- RatBot.user.js
+++ RatBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RatBot
 // @namespace    https://github.com/Shikster/RatBot.git
-// @version      1.9
+// @version      2.0
 // @description  Eep! I found someone!
 // @author       Shikii
 // @match        https://aberoth.com/*
@@ -12,35 +12,44 @@
 
 // Function to display a popup for adding names to the nameList
 function displayPasteNamesPopup() {
+    // Create a popup container
     const popupContainer = document.createElement('div');
-    popupContainer.style.position = 'fixed';
-    popupContainer.style.top = '50%';
-    popupContainer.style.left = '50%';
-    popupContainer.style.transform = 'translate(-50%, -50%)';
-    popupContainer.style.background = 'white';
-    popupContainer.style.padding = '20px';
-    popupContainer.style.border = '1px solid #ccc';
-    popupContainer.style.zIndex = '1000';
-    popupContainer.style.color = 'black';
+    popupContainer.style.cssText = `
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: white;
+        padding: 20px;
+        border: 1px solid #ccc;
+        z-index: 1000;
+        color: black;
+    `;
     document.body.appendChild(popupContainer);
 
+    // Close button
     const closeButton = document.createElement('button');
     closeButton.textContent = 'Close';
     closeButton.style.marginBottom = '10px';
     popupContainer.appendChild(closeButton);
 
+    // Text area for input
     const textArea = document.createElement('textarea');
     textArea.placeholder = 'Type or paste usernames separated by commas';
-    textArea.style.width = '100%';
-    textArea.style.height = '100px';
-    textArea.style.resize = 'none';
+    textArea.style.cssText = `
+        width: 100%;
+        height: 100px;
+        resize: none;
+    `;
     popupContainer.appendChild(textArea);
 
+    // Add button
     const addButton = document.createElement('button');
     addButton.textContent = 'Add to NameList';
     addButton.style.marginTop = '10px';
     popupContainer.appendChild(addButton);
 
+    // Event listeners
     closeButton.addEventListener('click', () => {
         document.body.removeChild(popupContainer);
     });
@@ -54,67 +63,60 @@ function displayPasteNamesPopup() {
         GM_setValue("NAME_LIST", nameList);
         document.body.removeChild(popupContainer);
     });
-	
-	textArea.addEventListener('keydown',(event)=>{
-		event.stopPropagation();
-	})	
-	
+
+    textArea.addEventListener('keydown', (event) => {
+        // Prevent event propagation
+        event.stopPropagation();
+    });
 }
 
 // Function to create the setup menu
 function createSetupMenu() {
     const setupContainer = document.createElement('div');
-    setupContainer.style.position = 'fixed';
-    setupContainer.style.top = '50%';
-    setupContainer.style.left = '50%';
-    setupContainer.style.transform = 'translate(-50%, -50%)';
-    setupContainer.style.background = 'white';
-    setupContainer.style.padding = '20px';
-    setupContainer.style.border = '1px solid #ccc';
-    setupContainer.style.zIndex = '1000';
-    setupContainer.style.color = 'black';
-    setupContainer.style.overflowY = 'auto';
+    setupContainer.style.cssText = `
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: white;
+        padding: 20px;
+        border: 1px solid #ccc;
+        z-index: 1000;
+        color: black;
+        overflow-y: auto;
+    `;
 
     setupContainer.innerHTML = `
         <!-- Your setup menu HTML -->
         <div style="max-height: 500px; overflow-y: auto;">
-        <h2>Information</h2>
-         <div>
-         <b>AFK ON/OFF:</b> When turned on, it will send a pause key up event every x minutes to prevent camping.
-         </div>
-         <div>
-         <b>DETECT ON/OFF:</b> Turns rat bot on or off.
-         </div>
-         <div>
-         <b>ADD NAMES:</b> Paste names you want to be skipped (whitelisted) like this: Name1, Name2, Name3,...
-         </div>
-         <div>
-         <b>SOUND:</b> Plays a radar sound when a player is detected by default, but can be changed in the audio field.
-         </div>
-        <h2>Setup</h2>
-        <label for="discordUrl">Discord Webhook URL:</label><br>
-        <input type="text" id="discordUrl" name="discordUrl" value="${GM_getValue("DISCORDURL", "")}" required><br><br>
-        <label for="myName">Your Scout Name:</label><br>
-        <input type="text" id="myName" name="myName" value="${GM_getValue("MY_NAME", "")}" required><br><br>
-        
-        <label for="discordUrl2">Discord Webhook URL 2 (Whitelisted Users):</label><br>
-        <input type="text" id="discordUrl2" name="discordUrl2" value="${GM_getValue("DISCORDURL2", "")}" required><br><br>
-		
-        <label for="mySound">Change audio. Full link to a .mp3 file:</label><br>
-        <input type="text" id="mySound" name="mySound" value="${GM_getValue("MY_AUDIO", "")}" required><br><br>
+            <h2>Information</h2>
+            <div><b>AFK ON/OFF:</b> When turned on, it will send a pause key up event every x minutes to prevent camping.</div>
+            <div><b>DETECT ON/OFF:</b> Turns rat bot on or off.</div>
+            <div><b>ADD NAMES:</b> Paste names you want to be skipped (whitelisted) like this: Name1, Name2, Name3,...</div>
+            <div><b>SOUND:</b> Plays a radar sound when a player is detected by default, but can be changed in the audio field.</div>
 
-        <h2>Add Names</h2>
-         <div>
-         <b>Adding players:</b> Click the button below to add names to the whitelist. Names on this list will be skipped from scouting. Every name that is not on this list will send a message to Discord and/or play a sound effect.
-         </div>
-        <br>
-        <button id="addNamesButton">Add names</button>
-        <br>
-        <br>
-        <button id="saveButton">Save</button>
+            <h2>Setup</h2>
+            <label for="discordUrl">Discord Webhook URL:</label><br>
+            <input type="text" id="discordUrl" name="discordUrl" value="${GM_getValue("DISCORDURL", "")}" required><br><br>
+            <label for="myName">Your Scout Name:</label><br>
+            <input type="text" id="myName" name="myName" value="${GM_getValue("MY_NAME", "")}" required><br><br>
+            <label for="discordUrl2">Discord Webhook URL 2 (Whitelisted Users):</label><br>
+            <input type="text" id="discordUrl2" name="discordUrl2" value="${GM_getValue("DISCORDURL2", "")}" required><br><br>
+            <label for="mySound">Change audio. Full link to a .mp3 file:</label><br>
+            <input type="text" id="mySound" name="mySound" value="${GM_getValue("MY_AUDIO", "")}" required><br><br>
+
+            <h2>Add Names</h2>
+            <div><b>Adding players:</b> Click the button below to add names to the whitelist. Names on this list will be skipped from scouting. Every name that is not on this list will send a message to Discord and/or play a sound effect.</div>
+            <br>
+            <button id="addNamesButton">Add names</button>
+            <br>
+            <br>
+            <button id="saveButton">Save</button>
+        </div>
     `;
 
     document.body.appendChild(setupContainer);
+
     const addNamesButton = setupContainer.querySelector('#addNamesButton');
     addNamesButton.addEventListener('click', () => {
         displayPasteNamesPopup();
@@ -122,25 +124,26 @@ function createSetupMenu() {
 
     const saveButton = setupContainer.querySelector("#saveButton");
     const discordUrlInput = setupContainer.querySelector("#discordUrl");
-    const discordUrlInput2 = setupContainer.querySelector("#discordUrl2"); 
+    const discordUrlInput2 = setupContainer.querySelector("#discordUrl2");
     const myNameInput = setupContainer.querySelector("#myName");
-    const myAudioInput = setupContainer.querySelector("#mySound")
+    const myAudioInput = setupContainer.querySelector("#mySound");
 
     saveButton.addEventListener("click", () => {
         const discordUrl = discordUrlInput.value;
-        const discordUrl2 = discordUrlInput2.value; 
+        const discordUrl2 = discordUrlInput2.value;
         const myName = myNameInput.value;
-	const mySound = myAudioInput.value;
+        const mySound = myAudioInput.value;
         GM_setValue("DISCORDURL", discordUrl);
-        GM_setValue("DISCORDURL2", discordUrl2); 
+        GM_setValue("DISCORDURL2", discordUrl2);
         GM_setValue("MY_NAME", myName);
-	GM_setValue("MY_AUDIO", mySound);
+        GM_setValue("MY_AUDIO", mySound);
         document.body.removeChild(setupContainer);
     });
-	
-	setupContainer.addEventListener('keydown',(event)=>{
-		event.stopPropagation();
-	})
+
+    setupContainer.addEventListener('keydown', (event) => {
+        // Prevent event propagation
+        event.stopPropagation();
+    });
 }
 
 // Create and initialize setup button and other UI elements
@@ -150,8 +153,9 @@ setupButton.style.width = '80px';
 document.body.appendChild(setupButton);
 
 setupButton.addEventListener("click", () => {
-createSetupMenu();   
- //window.location.href = "https://aberoth.com/download.html";
+    createSetupMenu();
+
+    // Check for a unique element on the download page
     setTimeout(() => {
         if (document.getElementById("uniqueElementOnDownloadPage")) {
             createSetupMenu();
@@ -160,20 +164,22 @@ createSetupMenu();
 });
 
 if (window.location.href === "https://aberoth.com/download.html") {
-	createSetupMenu();
+    createSetupMenu();
 }
 
 // Variables for controlling bot behavior
 let isDetecting = true;
 let useNameList = true;
 
-//Excluded list
-const excludedNames = ["Tavelor", "Gomald", "Orc", "Rogue", "Thief", 
-"Wolf", "Alchemist", "Bat", "Black Bat", "Disciple", "Forstyll", "Grand Shaman",
-"Lich", "Master Alchemist", "Master Thief", "Minotaur", "Ourik", "Plague Rat", "Rat",
-"Ratingar", "Rattle Snake", "Satyr", "Satyr Elder", "Shaman", "Skaldor", "Skeleton",
-"Skelet Rat", "Skelet Wolf", "Vampire Bat", "Inala", "Sholop", "Lysis", "Darklow", "Wodon",
-"Magerlin", "Gurun"];
+// Excluded list
+const excludedNames = [
+    "Tavelor", "Gomald", "Orc", "Rogue", "Thief",
+    "Wolf", "Alchemist", "Bat", "Black Bat", "Disciple", "Forstyll", "Grand Shaman",
+    "Lich", "Master Alchemist", "Master Thief", "Minotaur", "Ourik", "Plague Rat", "Rat",
+    "Ratingar", "Rattle Snake", "Satyr", "Satyr Elder", "Shaman", "Skaldor", "Skeleton",
+    "Skelet Rat", "Skelet Wolf", "Vampire Bat", "Inala", "Sholop", "Lysis", "Darklow", "Wodon",
+    "Magerlin", "Gurun"
+];
 
 // Toggle buttons for detecting and using the name list
 const toggleBtn = document.createElement('button');
@@ -201,18 +207,19 @@ function shouldDetect(username) {
 // Function to display the name list in a popup
 function displayNameListPopup() {
     const popupContainer = document.createElement('div');
-    popupContainer.style.position = 'fixed';
-    popupContainer.style.top = '50%';
-    popupContainer.style.left = '50%';
-    popupContainer.style.transform = 'translate(-50%, -50%)';
-    popupContainer.style.background = 'white';
-    popupContainer.style.padding = '20px';
-    popupContainer.style.border = '1px solid #ccc';
-    popupContainer.style.zIndex = '1000';
-    popupContainer.style.color = 'black';
-    popupContainer.style.overflow = 'auto';
-    popupContainer.style.maxHeight = '70vh';
-
+    popupContainer.style.cssText = `
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: white;
+        padding: 20px;
+        border: 1px solid #ccc;
+        z-index: 1000;
+        color: black;
+        overflow: auto;
+        max-height: 70vh;
+    `;
     document.body.appendChild(popupContainer);
 
     const closeButton = document.createElement('button');
@@ -226,8 +233,10 @@ function displayNameListPopup() {
     nameList.forEach(name => {
         const listItem = document.createElement('li');
         const nameContainer = document.createElement('div');
-        nameContainer.style.display = 'flex';
-        nameContainer.style.alignItems = 'center';
+        nameContainer.style.cssText = `
+            display: flex;
+            align-items: center;
+        `;
 
         const nameElement = document.createElement('span');
         nameElement.textContent = name;
@@ -236,6 +245,7 @@ function displayNameListPopup() {
         const removeButton = document.createElement('button');
         removeButton.textContent = 'X';
         removeButton.style.marginLeft = '10px';
+
         removeButton.addEventListener('click', () => {
             const index = nameList.indexOf(name);
             if (index !== -1) {
@@ -337,7 +347,6 @@ soundToggleButton.addEventListener('click', () => {
     soundToggleButton.textContent = isSoundEnabled ? 'SOUND ON' : 'SOUND OFF';
 });
 
-
 //Scouting for unknowns, and making screenshot
 function dataURLtoFile(dataurl, filename) {
     var arr = dataurl.split(','),
@@ -395,9 +404,6 @@ const postUser = (username) => {
     }
 }
 
-
-
-
 //Scouting allies
 function postSkippedName(username) {
     const DISCORDURL2 = GM_getValue("DISCORDURL2", "");
@@ -426,4 +432,3 @@ function postSkippedName(username) {
         }
     }
 }
-
